dff3605aa1db5c5044b93ccd2a6b5d8a543c5900
ys jeon <jweb8712@gmail.com>
2023-09-04 20:46:23 +0900

    table 수정 보완

--- asset/js/inc/footer.js
+++ asset/js/inc/footer.js
@@ -3,14 +3,6 @@ document.write(
 	<div class="footer">
 		<div class="inner">
 			<p>COPYRIGHT 2023&nbsp;&copy;MG새마을금고.&nbsp; ALL RIGHTS RESERVED.</p>
-			<div class="footer-link">
-				<button type="button" class="button-link">외부사이트목록 <i class="fa-solid fa-plus fa-color03"></i></button>
-				<ul>
-					<li><a href="#">목록1</a></li>
-					<li><a href="#">목록2</a></li>
-					<li><a href="#">목록3</a></li>
-				</ul>
-			</div>
 		</div>
 	</div>
 	`
